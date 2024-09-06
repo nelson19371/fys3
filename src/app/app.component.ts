facd602261f658ae3deef115799aaffdc64cbd56
nelson19371 <sandoval19371@gmail.com>
2024-09-06 18:38:25 -0400

    iconos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,51 @@ import { DataSharingService } from './data-sharing.service';
 export class AppComponent implements OnInit {
   isLoading: boolean = true;
   alerta = {
-    "lat": "-23.749273",
-    "lng": "-70.108439",
+    "lat":"-23.749273",
+    "lng":"-70.108439",
     "mapa": "https://maps.google.com/?t=h&q=-23.749273,-70.108439",
     "patente": "RBXB38",
     "tipo_alerta": "Fatiga y Somnolencia",
     "detalle_alerta": "Conductor Bostezando",
     "fecha": "03-09-2024 12:42:58",
-    "nombre_contacto": "John Connor",
+    "nombre_contacto": "Nelson Connor",
     "nombre_responsable": "Sin Asignar",
     "cargo_responsable": "",
     "empresa": "INGENIERIA ERR SPA",
-    "estado": "Por Gestionar"
+    "estado": "Por Gestionar",
+    "notificaciones": [
+        {
+            "nombre": "Nelson Connor",
+            "email": {
+                "to": "nelsonconnor@savinghumanity.org",
+                "status": "sent"
+            },
+            "telefono": "+56912345678",
+            "cargo": "Ataja Incendios",
+            "nivel": "1",
+            "sms": "sent",
+            "whatsapp": {
+                "status": "read",
+                "status_time": "03-09-2024 12:43:00"
+            }
+        },
+        {
+          "nombre": "Nelson Connor",
+          "email": {
+              "to": "nelsonconnor@savinghumanity.org",
+              "status": "read"
+          },
+          "telefono": "+56912345678",
+          "cargo": "Ataja Incendios",
+          "nivel": "1",
+          "sms": "sent",
+          "whatsapp": {
+              "status": "read",
+              "status_time": "03-09-2024 12:43:00"
+          }
+      },
+        // otros objetos de notificación...
+    ]
   };
 
   mapUrl: SafeResourceUrl;
@@ -35,24 +68,6 @@ export class AppComponent implements OnInit {
     this.dataSharingService.currentId.subscribe(id => {
       this.id = id;
       console.log("ID recibido en AppComponent:", this.id);
-
-        this.alerta = {
-          "lat": "-23.749273"+this.id,
-          "lng": "-70.108439"+this.id,
-          "mapa": "https://maps.google.com/?t=h&q=-23.749273,-70.108439 id:"+this.id,
-          "patente": "RBXB38 id:"+this.id,
-          "tipo_alerta": "Fatiga y Somnolencia id:"+this.id,
-          "detalle_alerta": "Conductor Bostezando id:"+this.id,
-          "fecha": "03-09-2024 12:42:58 id:"+this.id,
-          "nombre_contacto": "Nelson Connor id:"+this.id,
-          "nombre_responsable": "Sin Asignar id:"+this.id,
-          "cargo_responsable": " id:"+this.id,
-          "empresa": "INGENIERIA ERR SPA id:"+this.id,
-          "estado": "Por Gestionar id:"+this.id
-        };
-
-
-
     });
   }
 
@@ -60,5 +75,34 @@ export class AppComponent implements OnInit {
     const url = `https://maps.google.com/?t=h&q=${lat},${lng}&z=10&output=embed`;
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
-}
 
+  getEmailIconClass(status: string): string {
+    switch(status) {
+      case 'sent':
+        return 'fa-check'; // Green check for sent
+      case 'read':
+        return 'fa-check-double'; // Double check for read
+      case 'received':
+        return 'fa-check-double blue'; // Blue double check for received
+      default:
+        return '';
+    }
+  }
+
+  getSmsIconClass(smsStatus: string): string {
+    return smsStatus === 'sent' ? 'fa-check' : '';
+  }
+
+  getWhatsappIconClass(status: string): string {
+    switch(status) {
+      case 'sent':
+        return 'fa-check-double blue'; // Azul para "sent"
+      case 'read':
+        return 'fa-check-double'; // Doble check para "read"
+      case 'received':
+        return 'fa-check-double green'; // Verde para "received"
+      default:
+        return '';
+    }
+  }
+}
